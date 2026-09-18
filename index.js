b7e55ebb9226a98e8ff121b96d98555ef81e6d32
michaeloc1 <119686988+michaeloc1@users.noreply.github.com>
2026-09-18 19:07:03 -0400

    Update index.js

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const db = mysql.createConnection(
       // MySQL username,
       user: 'root',
       // MySQL password
-      password: '**45Hello01**',
+      
       database: 'employee_tracker_db'
     },
     console.log(`Connected to the employee_tracker_db database.`)
